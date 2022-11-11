5682cec1a36d12b3171063e98df59e489be60d9e
Denis Belokopytov <tomcreo@mail.ru>
2022-11-11 16:08:35 +0300

    Решили задача 00

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Task00 Напишите программу, которая на вход принимает число и возводит его в квадрат.
+        Console.WriteLine("Введите целое число");
 
+        int num = Convert.ToInt32(Console.ReadLine());
+        int square = num * num;
+        Console.WriteLine($"Квадрат числа {num} = {square}");
+    }
+}
